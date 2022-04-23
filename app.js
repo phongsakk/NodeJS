09db5863a1e96a9735b8faef177bb7dfbb1df52a
Phongsak Yotsaodee <54858537+phongsakk@users.noreply.github.com>
2022-04-23 16:14:55 +0700

    productsRouter as seperate file

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const productsRouter = require('./src/route/productsRouter');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-const productRouter = express.Router();
-
-const products = require('./src/data/products.json');
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, '/public/')));
@@ -22,20 +20,7 @@ app.get("/", (req, res) => {
     });
 });
 
-productRouter.route("/").get((req, res) => {
-    res.render('products', {
-        products: products
-    });
-});
-
-productRouter.route("/:id").get((req, res) => {
-    const id = req.params.id;
-    res.render('product', {
-        product: products[id],
-    });
-});
-
-app.use("/products", productRouter);
+app.use("/products", productsRouter);
 
 app.listen(PORT, () => {
     console.log('listening on port' + chalk.green(":" + PORT));
